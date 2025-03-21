a844d7bd1a3c26755d391fcf33f357bbcae326fb
niloofar yousefiafrouz <niloofar.yousefiafrouz@dci-student.org>
2025-03-21 23:46:14 +0100

    1

--- convex/http.ts
+++ convex/http.ts
@@ -1,5 +1,20 @@
 import { httpRouter } from "convex/server";
-
-
+import {httpAction} from "./_generated/server"
+import { request } from "http";
 
 const http = httpRouter();
+
+
+http.route({
+    path:"/clerk-webhook",
+    method: "POST",
+
+handle: httpAction(async(ctx,request)=>{
+    const webhookSecret = process.env.CLERK_WEBHOOK_SECRET;
+    if (!webhookSecret){
+        throw new Error("Missing CLERK_WEBHOOK_SECRET enviroment variable");
+    }
+})
+
+})
+
