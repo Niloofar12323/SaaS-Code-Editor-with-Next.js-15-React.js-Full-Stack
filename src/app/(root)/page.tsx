2c75567aa3428d25e8fd3d0a6c011f2072df7085
niloofar yousefiafrouz <niloofar.yousefiafrouz@dci-student.org>
2025-04-01 21:22:17 +0200

    1

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
    <div>header</div>
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">editor and preview
     </div>
-    <div></div>
+  
   </div>
   </div>
   ) ; 
