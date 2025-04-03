62c02045aae06a1119c2611c0ace77ba0a6922b9
niloofar yousefiafrouz <niloofar.yousefiafrouz@dci-student.org>
2025-04-04 01:55:34 +0200

    1

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,14 +1,16 @@
+import EditorPanel from "./_components/EditorPanel";
+import Header from "./_components/Header";
+import OutputPanel from "./_components/OutputPanel";
 
 export default function Home() {
   return (
 <div className="min-h-screen">
   <div className="max-w-[1800px] mx-auto p-4">
-   <div>header</div>
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">editor and preview
+  <Header />
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+    <EditorPanel />
+    <OutputPanel />
     </div>
-  <div>
-    <h1></h1>
-  </div>
   </div>
   </div>
   ) ; 
